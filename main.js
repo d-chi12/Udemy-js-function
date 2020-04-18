fd0c86524fcd664855c874050f6fbe1330b61787
USER <user@USERnoMacBook-ea.local>
2020-04-18 19:38:29 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+
+function hello(name, age) {
+    // let name = 'code Mafia';
+    console.log('hello' + name + age);
+    return name + age;
+}
+
+hello('code Mafia', 10);
+const returnVal = hello('code Mafia2', 20);
+console.log(returnVal);
+
+// 1 関数に渡される値:　引数:　argument
+// 4 関数で戻される値:　戻り値:　Return value
+// 関数名を持たない関数:　無名関数:　Anonymous function
